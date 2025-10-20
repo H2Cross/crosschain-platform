eae44f00dc000559dbaaccf3d3eb74f63b3fb052
kidultff <i@mmuaa.com>
2025-10-20 11:36:24 +0800

    fix:链枚举英文名称修正

--- src/main/java/com/tanklab/platform/common/CrosschainInfo.java
+++ src/main/java/com/tanklab/platform/common/CrosschainInfo.java
@@ -5,12 +5,12 @@ package com.tanklab.platform.common;
  * @date 2025/6/4 22:55
 **/
 public enum CrosschainInfo {
-    ETH("Ethereum",12001,"8086","以太坊"),
-    H2C("H2Chain",13001,"8087","海河智链"),
-    CMK("ChainMaker",11001,"8088","长安链"),
-    BuB("Bubi",14001,"8089","布比链"),
-    FAB("Fabric",15001,"8090","Fabric"),
-    FBC("Fisco Bcos",16001,"8091","金链盟")
+    ETH("ethereum",12001,"8086","以太坊"),
+    H2C("h2chain",13001,"8087","海河智链"),
+    CMK("chainmaker",11001,"8088","长安链"),
+    BuB("bubi",14001,"8089","布比链"),
+    FAB("fabric",15001,"8090","Fabric"),
+    FBC("fisco",16001,"8091","金链盟")
     ;
     public String ChainName;
     public Integer ChainId;
